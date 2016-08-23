aaa49bc2bce0ee7f3c1116223d946f5f75ff83e2
Siddharth Pai <siddharth@10i.co.in>
2016-08-23 19:53:33 +0530

    Fixed issue with the edge cases

--- app/script.js
+++ app/script.js
@@ -1,35 +1,50 @@
+var STORAGE_AREA = 'urlSet'
+
 function saveURL(url) {
   var storageArea = chrome.storage.sync;
-
-  storageArea.get('urlSet',function(data){
-    if(data.length===0||data===undefined){
+  var urlSet = [];
+  storageArea.get(STORAGE_AREA,function(data){
+    if(data[STORAGE_AREA]===undefined){
       urlSet = [];
     }
-    data.urlSet.push(url);
-    storageArea.set({'urlSet': data.urlSet}, function() {
+    else{
+      urlSet = data[STORAGE_AREA];
+    }
+    urlSet.push(url);
+    var toBeStored = {};
+    toBeStored[STORAGE_AREA] = urlSet;
+    storageArea.set(toBeStored, function(data) {
+      console.log('What happen\'d on saving ? ',data);
     });
   });
 }
 
 function getURL(callback){
   var storageArea = chrome.storage.sync;
-  storageArea.get('urlSet',function(urlSet){
-    callback(urlSet);
+  storageArea.get(STORAGE_AREA,function(data){
+    if(data[STORAGE_AREA] !== undefined){
+      callback(data[STORAGE_AREA]);
+      }
+      else{
+        callback([]);
+      }
   })
 }
 
 var app=angular.module('workEX',[]);
+
 app.controller('mainCtrl',['$scope','$timeout',function($scope,$timeout){
-  debugger;
   $scope.savedURLS  = [];
+
   $scope.onSubmit = function(){
     $scope.savedURLS.push($scope.url);
     saveURL($scope.url);
   }
+  console.log('gonna get URLS');
   getURL(function(data){
-    $timeout(function(){
-      $scope.savedURLS = data.urlSet;
-    },0);
+      $timeout(function(){
+        $scope.savedURLS = data;
+      },0);
   });
   $scope.openPinTabs = function(){
     angular.forEach($scope.savedURLS,function(value,key){
